259da9defc027577d2bf80b8f313d5a8a6026db5
Joren <97386268+Aebal34@users.noreply.github.com>
2023-05-18 18:54:43 +0200

    n1exercici2 done

---
+++ n1exercici2/App.java
@@ -0,0 +1,31 @@
+package n1exercici2;
+
+import java.util.*;
+public class App {
+
+	public static void main(String[] args) {
+
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		LinkedList<Integer> invertedList = new LinkedList<Integer>();
+		
+		//We instantiate an iterator for each list
+		ListIterator<Integer> it = list.listIterator(list.size()); //Starts at last position
+		ListIterator<Integer> invertedIt = list.listIterator(); 
+		
+		while(it.hasPrevious()) {
+			
+			invertedList.add(it.previous());
+		}
+
+		System.out.println(list);
+		System.out.println(invertedList);
+	}
+
+}
